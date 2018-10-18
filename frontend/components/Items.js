b2d4b2d8c8bf2cd05b875f085d977942cc9d6014
Shawn Stern <shawn.stern@nike.com>
2018-10-18 14:43:32 -0700

    Updating Items query to accept pagination input (skip, first, order) and updating app to display most recently created items first. Pagination next/prev buttons now work and modify which 4 items are displayed at a time

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 export const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -33,7 +34,7 @@ const ItemsList = styled.div`
 const Items = ({ page }) => (
   <Center>
     <Pagination page={page} />
-    <Query query={ALL_ITEMS_QUERY}>
+    <Query query={ALL_ITEMS_QUERY} variables={{ skip: page * perPage - perPage, first: perPage }}>
       {({ data, error, loading }) => {
         if (loading) return <p>Loading...</p>;
         if (error) return <p>Error: {error.message}</p>;
