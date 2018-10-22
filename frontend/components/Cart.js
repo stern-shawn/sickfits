c8210dd968ddc0fed3e5bf17fb3e02fc8e65125f
Shawn Stern <shawn.stern@nike.com>
2018-10-22 12:23:08 -0700

    Updating cart UI to show user's cart items in detail, with aggregate prices per item + total price of the cart
    - Updating the User component's underlying CURRENT_USER_QUERY to also grab the users cart, with nested item query to get each item's details that are relevant to the cart
    - Wrapping the Cart in the User render prop component so user's cart data is available, and can be rendered with money formatting, quantities, images, etc + styling and dynamic formatting based on values
    - Add Item to Cart button now also reruns the user query so that user cart values are up to date, as well as disabling itself/showing 'adding' text while the add mutation is in flight

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -5,6 +5,10 @@ import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
+import User from './User';
+import CartItem from './CartItem';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import formatMoney from '../lib/formatMoney';
 
 // Query the client-only part of cache using the @client directive
 export const CART_QUERY = gql`
@@ -21,27 +25,45 @@ export const TOGGLE_CART_MUTATION = gql`
 `;
 
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>
-    {toggleCart => (
-      <Query query={CART_QUERY}>
-        {({ data: { cartOpen } }) => (
-          <CartStyles open={cartOpen}>
-            <header>
-              <CloseButton title="close" onClick={toggleCart}>
-                &times;
-              </CloseButton>
-              <Supreme>Your Cart</Supreme>
-              <p>You have __ items in your cart.</p>
-            </header>
-            <footer>
-              <p>$10.10</p>
-              <SickButton>Checkout</SickButton>
-            </footer>
-          </CartStyles>
-        )}
-      </Query>
-    )}
-  </Mutation>
+  <User>
+    {({ data: { me } }) => {
+      if (!me) return null;
+      return (
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+          {toggleCart => (
+            <Query query={CART_QUERY}>
+              {({ data: { cartOpen } }) => (
+                <CartStyles open={cartOpen}>
+                  <header>
+                    <CloseButton title="close" onClick={toggleCart}>
+                      &times;
+                    </CloseButton>
+                    <Supreme>
+                      {me.name}
+                      's Cart
+                    </Supreme>
+                    <p>
+                      You have {me.cart.length} item
+                      {me.cart.length === 1 ? '' : 's'} in your cart.
+                    </p>
+                  </header>
+                  <ul>
+                    {me.cart.map(cartItem => (
+                      <CartItem key={cartItem.id} cartItem={cartItem} />
+                    ))}
+                  </ul>
+                  <footer>
+                    <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+                    <SickButton>Checkout</SickButton>
+                  </footer>
+                </CartStyles>
+              )}
+            </Query>
+          )}
+        </Mutation>
+      );
+    }}
+  </User>
 );
 
 export default Cart;
